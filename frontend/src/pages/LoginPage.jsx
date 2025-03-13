aeb8a485e538f245455506d651be9c313767b9c2
Prahlad Kumar <prahlad@Mac.lan>
2025-03-13 20:13:54 +0530

    login password length chec removed

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -17,7 +17,6 @@ const LoginPage = () => {
     if(!formData.email.trim())return toast.error("Email is required");
     if(!/\S+@\S+\.\S+/.test(formData.email))return toast.error("Invalid email format");
     if(!formData.password)return toast.error("Password is required");
-    if(formData.password.length < 6)return toast.error("Password must be at least 6 characters");
     return true;
 }
   const handleSubmit = (e) => {
